89b1292f83f523c3f0dc2cc4bc8aef78e45c46fb
hibikiitoga <itogahibiki@gmail.com>
2018-05-03 13:27:32 +0900

    calc and averaging correlations

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+   Copyright: Itoga and Takeuchi
+   License: MIT (for all files)
+*/
+
+
 #include "PDB.hpp"
 #include "Kahan_summation_algorithm.hpp"
 
@@ -35,7 +41,7 @@ int main(int argc, char* argv[])
          if("time"==vs.at(0))
          {
             std::vector<std::string> vs_;
-            boost::algotrithm::split(vs_,vs.at(1),boost::is_any_of("-"));
+            boost::algorithm::split(vs_,vs.at(1),boost::is_any_of("-"));
             time_range=std::tuple<double,double>
             (
                boost::lexical_cast<double>(vs_.at(0)),
@@ -45,16 +51,45 @@ int main(int argc, char* argv[])
       }
    }catch(...)
    {
-      std::cout<<"Failure @input argments"<<std::endl;   
+      std::cout<<"Failure @input argments"<<std::endl;exit(0);
    }
 
-   std::vector<Protein> proteins = load(input_file_name);
+   std::vector<Protein> proteins = load(input_file_name,time_range);
 
+   if(proteins.size()<2){std::cout<<"The number of samples is insufficient."<<std::endl;exit(0);}
+
+   const int first_res = proteins.at(0).get_index_of_first_resudue();
+   const int last_res  = proteins.at(0).get_index_of_last_resudue();
+   const int pos_target = target_residue_index-first_res;
+   std::vector<Kahan> correlations(last_res-first_res+1);
    for(size_t i=0,i_size=proteins.size();i<i_size;++i)
    {
-      
+      std::vector<Vector3D> us;//include disp. of each residue
+      for(int r=first_res;r<=last_res;++r)
+      {
+         us.push_back(displacement_of_residue(proteins.at(i),proteins.at(i+1),r));
+      }
+      const int pos_target = target_residue_index-first_res;
+      const Vector3D& u_i = us.at(pos_target);
+      for(size_t r=0,r_size=us.size();r<r_size;++r)
+      {
+         const Vector3D& u_j = us.at(r);
+         correlations.at(r) += correlation(u_i,u_j);
+      }
    }
-
+   std::ofstream ofs(output_file_name,std::ios::trunc);
+   ofs<<"# Res.index <u_i*u_j>_t"<<std::endl;
+   for(size_t i=0,i_size=correlations.size();i<i_size;++i)
+   {
+      if(i!=target_residue_index)
+      {
+         ofs<<i+first_res<<" "<<correlations.at(i).get_av();
+      }else
+      {
+         ofs<<std::endl<<std::endl;
+      }
+   }
+   ofs.close();
    return EXIT_SUCCESS;
 }
 
